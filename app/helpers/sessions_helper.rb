c0884c3ce147c265491f58be25ac69274c1ac538
hungxuan <nguyen.xuan.hung@framgia.com>
2018-05-24 17:33:59 +0700

    View profile user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -31,6 +31,10 @@ module SessionsHelper
     cookies.delete :remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def log_out
     forget current_user
     session.delete :user_id
