add5184706ebebc9f2164c4b542637ec323bbff0
huuhau <nguyenhuuhau1061995@gmail.com>
2018-06-04 14:19:45 +0700

    view detail product with multiple images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604084957) do
+ActiveRecord::Schema.define(version: 20180604044135) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
