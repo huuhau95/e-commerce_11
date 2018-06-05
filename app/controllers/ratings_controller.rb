bb960c955bba37cccefc527d94d8e140259e850f
huuhau <nguyenhuuhau1061995@gmail.com>
2018-06-05 10:23:42 +0700

    create rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,6 +1,7 @@
 class RatingsController < ApplicationController
   before_action :load_menu, :set_search_product
   before_action :params_rating_id, only: :update
+  before_action :product_id_params
 
   def create
   	@rating = current_user.ratings.build params_rating
