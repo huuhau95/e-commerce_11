a6388109df04aa0e4d95564fbdf2d1b84681c5b0
hungxuan <nguyen.xuan.hung@framgia.com>
2018-06-05 18:08:54 +0700

    backend1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,7 @@ Rails.application.configure do
   # test send mail
   # config.action_mailer.default_url_options = { :host => "localhost:3000" }
   host = ENV["DEVELOPMENT"]
+
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
