c7e8cde1413c4a6286b5e9e4d9f9713e5ca227cc
hungxuan <nguyen.xuan.hung@framgia.com>
2018-06-06 13:16:35 +0700

    add edit users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,6 @@
 class Admin::UsersController < Admin::BaseController
   before_action :logged_in_user
   before_action :load_user, only: %i(show edit update destroy)
-<<<<<<< 086d526406dbabc07cfd9a711e7dd13f8466571d
 
   def index
     @q = User.ransack(params[:q])
@@ -26,31 +25,6 @@ class Admin::UsersController < Admin::BaseController
 
   def edit; end
 
-=======
-
-  def index
-    @users = User.user_info.page(params[:page]).per Settings.settings.per_page
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new user_params
-    if @user.save
-      flash[:info] = t "create_success"
-      redirect_to admin_users_url
-    else
-      render :new
-    end
-  end
-
-  def show; end
-
-  def edit; end
-
->>>>>>> view users
   def update
     if @user.update user_params
       flash[:success] = t "profile_updated"
@@ -60,26 +34,17 @@ class Admin::UsersController < Admin::BaseController
     end
   end
 
-<<<<<<< 086d526406dbabc07cfd9a711e7dd13f8466571d
   def destroy
     @user.destroy
     flash[:success] = t "delete_success"
     redirect_to admin_users_url
   end
-=======
-  def destroy; end
->>>>>>> view users
 
   private
 
   def user_params
-<<<<<<< 086d526406dbabc07cfd9a711e7dd13f8466571d
     params[:user][:role] = params[:user][:role].to_i
     params.require(:user).permit :name, :email, :image, :password, :role, :password_confirmation
-=======
-    params.require(:user).permit :name, :email, :password, :images, :role,
-      :password_confirmation
->>>>>>> view users
   end
 
   def load_user
