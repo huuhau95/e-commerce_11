35f820ee40541b40cfa39ae43de386d8066c7a71
hungxuan <nguyen.xuan.hung@framgia.com>
2018-06-06 11:45:37 +0700

    backend1

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :order_details
+  enum status: {pending: 0, checked: 1}
+  has_many :order_details, dependent: :destroy
+  default_scope -> { order created_at: :desc }
+  scope :filter_by_status, ->(status){where(status: status) if status.present?}
   validates :phone, presence: true, numericality: {only_integer: true}
   validates :address, presence: true, length: {maximum: Settings.settings.max_value_address,
     minimum: Settings.settings.min_value_address}
