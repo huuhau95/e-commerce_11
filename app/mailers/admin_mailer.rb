f7adf596009b41750b0c8ab4f1bf63dbccbcda11
hungxuan <nguyen.xuan.hung@framgia.com>
2018-06-12 16:16:26 +0700

    filter gem

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -7,8 +7,8 @@ class AdminMailer < ApplicationMailer
   end
 
   def load_statistic admin
-    @total_price = Product.total_this_month(:price)
-    @total_quantity = Product.total_this_month(:quantity)
+    @total_price = Product.total_this_month :price
+    @total_quantity = Product.total_this_month :quantity
     @admin = admin
     mail to: @admin.email, subject: "Monthly_statistic"
   end
